00c050e0adbcfe6d5fd49b477219119ce256a880
Souissi Skander <souissiskander494@gmail.com>
2026-03-31 12:39:22 +0100

    create project

---
+++ src/main/java/com/example/agencehotel/AgenceHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.agencehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenceHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgenceHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agencehotel/AgenceHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agencehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenceHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
